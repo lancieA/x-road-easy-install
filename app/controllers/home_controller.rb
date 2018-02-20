75d73570d3700e3e7143ce1b16ad71a9e7b27cf5
AndrewLnc <lancie_a@etna-alternance.net>
2018-02-20 15:08:50 +0100

    new form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,24 @@
 class HomeController < ApplicationController
   
+  def index
+  	@information = Information.new
+  	puts "index"
+  end
+
+  def show
+  	#@information = Information.new
+  end
+
+  def new
+  	
+  end
+
+  def update
+  	puts "update"
+  end
+
+  def create
+  	puts "create"
+  end
+
 end
